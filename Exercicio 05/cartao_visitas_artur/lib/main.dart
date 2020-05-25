bc5382e1842beeb4346202bb6ece2d86847bfdee
Artur Rocha <defteros7@gmail.com>
2020-05-25 16:29:49 -0300

    github icon

--- Exercicio 05/cartao_visitas_artur/lib/main.dart
+++ Exercicio 05/cartao_visitas_artur/lib/main.dart
@@ -10,18 +10,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueAccent,
         body: SafeArea(child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             buildAvatar(50, "images/example.png"),
             Text("Artur Rocha",
-              style: TextStyle(color: Colors.black,
+              style: TextStyle(color: Colors.white,
                   fontSize: 40,
                   fontFamily: "Satisfy",
                   fontWeight: FontWeight.bold),
             ),
             Text("BEGINNER FLUTTER DEVELOPER",
-              style: TextStyle(color: Colors.black,
+              style: TextStyle(color: Colors.white,
                   fontSize: 20,
                   letterSpacing: 1),
             ),
@@ -36,15 +37,24 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 buildAvatar(30, "images/tt.png"),
-                buildAvatar(30, "images/fb.jpg"),
-                buildAvatar(30, "images/insta.png")
+                buildSizedbox(10),
+                buildAvatar(30, "images/fb.png"),
+                buildSizedbox(10),
+                buildAvatar(30, "images/insta.png"),
               ],
+            ),
+            SizedBox(
+              height: 20,
+              width: 150,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 buildAvatar(30, "images/cel.png"),
-                buildAvatar(30, "images/email.png")
+                buildSizedbox(10),
+                buildAvatar(30, "images/mail.png",),
+                buildSizedbox(10),
+                buildAvatar(30, "images/git.png")
               ],
             )
           ],
@@ -55,5 +65,17 @@ class MyApp extends StatelessWidget {
 
   CircleAvatar buildAvatar(double radius, String image){
       return CircleAvatar(radius: radius, backgroundImage: AssetImage(image),);
+  }
+
+  SizedBox buildSizedbox(double w){
+    return SizedBox( width: w);
+  }
+
+  void _showToast(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text('Added to favorite'),
+    ));
   }
 }
